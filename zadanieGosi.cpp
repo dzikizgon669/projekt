3b0c416655c5655198023746d2f47ea748eb0e76
dzikizgon669 <edyta.janczak99@gmail.com>
2022-02-15 18:23:18 +0100

    First Commit

---
+++ zadanieGosi.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int bezwzgledna(int n)
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+	return n;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << "Wartosc bezwzgledna liczby: " << n << " to: " << bezwzgledna(n);
+	return 0;
+}///////Dopisuje dowolny komentarz do zadania kolezanki.
+
